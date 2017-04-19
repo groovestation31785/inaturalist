cf73a14b736599c02ebe91daf7bf95130be8be5b
groovestation31785 <email": null>
2017-04-19 13:54:27 -0700

    Use RestClient to retrieve data from API

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,3 +1,15 @@
+require 'rubygems'
+require 'rest-client'
+require 'json'
+
+# All of these endpoints should use https://www.inaturalist.org as the base URL, particularly endpoints that require auth.
+
+base_url = "https://www.inaturalist.org"
+
 get '/' do
+  response = RestClient.get("#{base_url}/observations.json")
+  parsed_response = JSON.parse(response,:symbolize_names => true)
+  puts parsed_response
+
   erb :index
 end
