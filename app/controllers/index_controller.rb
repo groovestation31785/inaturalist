9b83c4ee8de0dc7da5746174c0a55f014e7c0da5
groovestation31785 <email": null>
2017-04-19 18:50:17 -0700

    Continue working with getting the data from API and displaying it

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,17 +5,24 @@ require 'json'
 # All of these endpoints should use https://www.inaturalist.org as the base URL, particularly endpoints that require auth.
 
 get '/' do
+  p "TAXAAAAAA GET"
+  p @taxa
   erb :index
 end
 
 post '/results' do
-  response = RestClient.get("https://www.inaturalist.org/observations.json?q=#{params['query']}")
+  url = "https://www.inaturalist.org/observations.json?q=#{params['q']}"
+  response = RestClient.get(url)
   parsed_response = JSON.parse(response,:symbolize_names => true)
-  p params
-  puts "**********************************"
-  parsed_response.each do |hash|
-    puts hash[:taxon]
-    puts hash[:iconic_taxon]
+
+  @taxa = []
+  parsed_response.each do |creature_hash|
+    @taxa << creature_hash[:taxon]
   end
+
+  p "TAXAAAAA POST"
+  p @taxa.length
+  
+
   redirect '/'
 end
