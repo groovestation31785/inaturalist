def15f349fc94b0e1f5a4fef203e57ec839129c4
Pat Holland <pholls@users.noreply.github.com>
2017-04-20 11:52:52 -0700

    Update get request to hit the API

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -5,14 +5,8 @@ require 'json'
 # All of these endpoints should use https://www.inaturalist.org as the base URL, particularly endpoints that require auth.
 
 get '/' do
-  p "TAXAAAAAA GET"
-  p @taxa
-  erb :index
-end
-
-post '/results' do
-  url = "https://www.inaturalist.org/observations.json?q=#{params['q']}"
-  response = RestClient.get(url)
+  # url = "https://www.inaturalist.org/observations.json?q=#{params['q']}"
+  response = RestClient.get("https://www.inaturalist.org/observations.json")
   parsed_response = JSON.parse(response,:symbolize_names => true)
 
   @taxa = []
@@ -20,9 +14,25 @@ post '/results' do
     @taxa << creature_hash[:taxon]
   end
 
-  p "TAXAAAAA POST"
-  p @taxa.length
-  
 
-  redirect '/'
+  # @taxa.each do |t|
+  #   if !t.nil?
+  #     p "*************************"
+  #     p t[:rank]
+  #   end
+  # end
+  erb :index
 end
+
+# post '/results' do
+#   url = "https://www.inaturalist.org/observations.json?q=#{params['q']}"
+#   response = RestClient.get(url)
+#   parsed_response = JSON.parse(response,:symbolize_names => true)
+#
+#   @taxa = []
+#   parsed_response.each do |creature_hash|
+#     @taxa << creature_hash[:taxon]
+#   end
+#
+#   redirect '/'
+# end
