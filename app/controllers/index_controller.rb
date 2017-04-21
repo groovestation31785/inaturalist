3f759c88bdb5ed72af7a3fcdb26872965de96386
Pat Holland <pholls@users.noreply.github.com>
2017-04-20 19:44:15 -0700

    Refactor controller to call private methods; leaner request

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -8,32 +8,32 @@ get '/' do
   response = RestClient.get("https://www.inaturalist.org/observations.json")
   parsed_response = JSON.parse(response,:symbolize_names => true)
 
-  @organisms = []
-  parsed_response.each do |organism_hash|
-    if organism_hash[:taxon] != nil
-      if organism_hash[:iconic_taxon] != nil
-        @organisms << Organism.new(organism_hash)
-      end
-    end
-  end
-
-
-  @iconic_taxon_names = ["Plantae", "Animalia", "Mollusca", "Reptilia", "Aves", "Amphibia", "Actinopterygii", "Mammalia", "Insecta", "Arachnida", "Fungi", "Protozoa", "Chromista", "Unknown"]
-
-  @iconic_taxon_names.size.times do |name|
-    @iconic_taxon_names[name] = @organisms.select { |org| org.iconic_taxon_name == @iconic_taxon_names[name] }
-  end
-
-  @organisms
-  @iconic_taxon_names
-
-
-  @iconic_taxon_names.each do |taxon|
-     if !taxon.empty?
-       p "*********************"
-       p taxon
-     end
-   end
+  @organisms = list_organisms(parsed_response)
+  @iconic_taxon_names = sort_taxon(@organisms)
 
   erb :index
 end
+
+private
+
+  def list_organisms(json_response)
+    organisms = []
+    json_response.each do |organism_hash|
+      if organism_hash[:taxon] != nil
+        if organism_hash[:iconic_taxon] != nil
+          organisms << Organism.new(organism_hash)
+        end
+      end
+    end
+    organisms
+  end
+
+
+  def sort_taxon(organisms)
+    iconic_taxon_names = ["Plantae", "Animalia", "Mollusca", "Reptilia", "Aves", "Amphibia", "Actinopterygii", "Mammalia", "Insecta", "Arachnida", "Fungi", "Protozoa", "Chromista", "Unknown"]
+
+    iconic_taxon_names.size.times do |name|
+      iconic_taxon_names[name] = organisms.select { |org| org.iconic_taxon_name == iconic_taxon_names[name] }
+    end
+    iconic_taxon_names
+  end
